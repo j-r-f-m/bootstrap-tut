0a89470fd47a325b34cb0b63069e3d7ec4bf6ca5
jrfm <moesch.jonas@protonmail.com>
2023-05-21 14:22:02 +0200

    Row examples

--- src/pages/Grid.jsx
+++ src/pages/Grid.jsx
@@ -18,69 +18,7 @@ export default function Grid() {
         rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
         ipsum dolor sit amet.
       </p>
-      <h2 className="container">Columns Example</h2>
-      <div className="container">
-        <div className="row mb-4">
-          <div className="col-auto">
-            <div className="box">text</div>
-          </div>
-          <div className="col-4">
-            <div className="box"></div>
-          </div>
-          <div className="col">
-            <div className="box"></div>
-          </div>
-          <div className="col">
-            <div className="box"></div>
-          </div>
-        </div>
 
-        {/* responsive - using breakpoints */}
-        <div className="row mb-4">
-          {/* for all screen sizes large and above the column will take up 4
-          units of space. below large screen sizes the column will take up 8 
-          units of space */}
-          <div className="col-lg-4 col-8">
-            <div className="box">responsive</div>
-          </div>
-          <div className="col-4">
-            <div className="box"></div>
-          </div>
-        </div>
-
-        {/* offset */}
-        <div className="row mb-4">
-          <div className="col-3">
-            <div className="box"></div>
-          </div>
-          <div className="col-3 offset-3">
-            <div className="box">offset</div>
-          </div>
-        </div>
-      </div>
-
-      <h2 className="container">Row Example</h2>
-      <div className="container">
-        <div className="row row-cols-2 row-cols-lg-4 mb-4">
-          <div className="col">
-            <div className="box"></div>
-          </div>
-          <div className="col">
-            <div className="box"></div>
-          </div>
-          <div className="col">
-            <div className="box"></div>
-          </div>
-          <div className="col">
-            <div className="box"></div>
-          </div>
-          <div className="col">
-            <div className="box"></div>
-          </div>
-        </div>
-      </div>
-
-      <h2 className="container">react comps</h2>
       <h2 className="container">Columns Example</h2>
       <Container>
         {/* basic column use */}
@@ -110,7 +48,7 @@ export default function Grid() {
         </Row>
 
         {/* Offset */}
-        <Row className="mb">
+        <Row className="mb-4">
           <Col xs={3}>
             <div className="box"></div>
           </Col>
@@ -120,6 +58,48 @@ export default function Grid() {
           </Col>
         </Row>
       </Container>
+
+      <h2 className="container">Row Example</h2>
+      <Container className="mb-4">
+        <Row xs={2} lg={4} className="gy-5">
+          <Col>
+            <div className="box"></div>
+          </Col>
+          <Col>
+            <div className="box"></div>
+          </Col>
+          <Col>
+            <div className="box"></div>
+          </Col>
+          <Col>
+            <div className="box"></div>
+          </Col>
+          <Col>
+            <div className="box"></div>
+          </Col>
+        </Row>
+      </Container>
+
+      {/* <h2 className="container">Row Example</h2>
+      <div className="container">
+        <div className="row row-cols-2 row-cols-lg-4 mb-4">
+          <div className="col">
+            <div className="box"></div>
+          </div>
+          <div className="col">
+            <div className="box"></div>
+          </div>
+          <div className="col">
+            <div className="box"></div>
+          </div>
+          <div className="col">
+            <div className="box"></div>
+          </div>
+          <div className="col">
+            <div className="box"></div>
+          </div>
+        </div>
+      </div> */}
     </>
   );
 }
